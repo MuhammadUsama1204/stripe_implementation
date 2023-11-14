b83f039649f3b53de8eb48acfb08d3fe454b9f7e
MuhammadUsama1204 <Muhammad.Usama@xprolabs.com>
2023-11-14 14:49:00 +0500

    product: add destroy action for delete products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,13 +22,21 @@ class ProductsController < ApplicationController
   def show; end
 
   def edit; end
-  
+
   def update
     if @product.update(product_params)
       flash[:success] = "Successfully Updated"
       redirect_to products_path
     else
       render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      redirect_to products_path, notice: "Product was successfully destroyed."
+    else
+      redirect_to products_path, alert: "Unable to destroy Product."
     end
   end
 
